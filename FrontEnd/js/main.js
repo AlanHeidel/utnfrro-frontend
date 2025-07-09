e9253d173861c725a566f08239d92804144b34db
alanheidel <alanmax2015.aheidel@gmail.com>
2025-07-09 10:50:00 -0300

    navbar responsive

---
+++ FrontEnd/js/main.js
@@ -0,0 +1,24 @@
+
+const checkbox = document.getElementById('check');
+const navbarList = document.querySelector('.navbar-list');
+const body = document.body;
+
+window.addEventListener('scroll', () => {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 10) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+})
+
+  checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+      navbarList.classList.add('open');
+      body.classList.add('no-scroll');
+    } else {
+      navbarList.classList.remove('open');
+      body.classList.remove('no-scroll');
+    }
+  });
+
